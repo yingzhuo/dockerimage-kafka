c9815236b079d7a5f0451774fc716886cc25b595
yingzhuo <yingzhor@gmail.com>
2019-11-20 11:55:05 +0800

    2019-11-20 11:55:05

--- 2.3.1/docker-entrypoint.sh
+++ 2.3.1/docker-entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+set -e
+
+docktool filegen -t="/opt/server.properties.template" -o="/opt/kafka/config/server.properties"
+
 bash /opt/zookeeper/bin/zkServer.sh start
 
 bash /opt/kafka/bin/kafka-server-start.sh /opt/kafka/config/server.properties
