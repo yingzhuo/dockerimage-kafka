ee0d5182a465a22ebd4960037b3c5605746834dd
yingzhuo <yingzhor@gmail.com>
2019-11-25 09:35:51 +0800

    2019-11-25 09:35:51

--- 2.3.1/docker-entrypoint.sh
+++ 2.3.1/docker-entrypoint.sh
@@ -2,10 +2,8 @@
 
 set -e
 
-docktool --quiet test --env="KAFKA_ADVERTISED_PORT"
-
+docktool --quiet test --env="KAFKA_ADVERTISED_HOST_NAME" --env="KAFKA_ADVERTISED_PORT"
 docktool --quiet filegen -t="/opt/server.properties.tmpl" -o="/opt/kafka/config/server.properties"
-
-bash /opt/zookeeper/bin/zkServer.sh start
+docktool --quiet wait -w "$KAFKA_ZOOKEEPER_CONNECT"
 
 bash /opt/kafka/bin/kafka-server-start.sh /opt/kafka/config/server.properties
