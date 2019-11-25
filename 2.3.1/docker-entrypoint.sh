81acaac3e8398bfef905115a2736dfc193b64351
yingzhuo <yingzhor@gmail.com>
2019-11-26 07:52:31 +0800

    2019-11-26 07:52:31

--- 2.3.1/docker-entrypoint.sh
+++ 2.3.1/docker-entrypoint.sh
@@ -1,6 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
+
+mkdir -p /var/kafka-logs
 
 docktool --quiet test --env="KAFKA_ADVERTISED_HOST_NAME" --env="KAFKA_ADVERTISED_PORT"
 docktool --quiet filegen -t="/opt/server.properties.tmpl" -o="/opt/kafka/config/server.properties"
